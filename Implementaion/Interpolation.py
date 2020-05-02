8cebd659401622f7ffbf4090e218880f94a87e4a
Mina Ashraf <minachraf@outlook.com>
2020-05-02 16:53:30 +0200

    Lagrange

--- Implementaion/Interpolation.py
+++ Implementaion/Interpolation.py
@@ -1,25 +1,62 @@
 import numpy
 from math import *
+
+
 class Interpolation:
-    def __init__(self, expression):
-        self.expression = expression
 
-    def f(self, x):
-        return self.evaluate(x)
-
-    def evaluate(self, x):
-        return eval(self.expression)
-
-    def newton_coeff(self, function, xes):
-        yes = numpy.empty(0)
-        list.sort(xes)
-        xes = numpy.array(xes)
+    def newton_coeff(self, xes, yes):
+        xes = numpy.copy(xes)
+        yes = numpy.copy(yes)
         l = len(xes)
-        for i in range(l):
-            yes = numpy.append(yes, round(self.evaluate(i)))
-            continue
         for k in range(1, l):
-            yes[k:l] = (yes[k:l] - yes[k - 1])/(xes[k:l] - xes[k - 1])
+            yes[k:l] = (yes[k:l] - yes[k - 1]) / (xes[k:l] - xes[k - 1])
         return yes
-#https://stackoverflow.com/questions/14823891/newton-s-interpolating-polynomial-python
-#with 2 votes
+
+    def Newton(self, xes, yes, X):
+        bubbleSortxy(xes, yes)
+        coefficient = self.newton_coeff(xes, yes)
+        degree = len(xes) - 1
+        answer = []
+        y = coefficient[degree]
+        string = str(y)
+        for x in X:
+            for i in range(1, degree + 1):
+                y = coefficient[degree - i] + (x - xes[degree - i]) * y
+                string = '('+str(coefficient[degree - i])+'(x-'+str(xes[degree - i])+')'+string+')'
+                continue
+            answer.append(y)
+            continue
+        return [answer, string]
+# https://stackoverflow.com/questions/14823891/newton-s-interpolating-polynomial-python
+# with 2 votes
+def bubbleSortxy(arrx,arry):
+    n = len(arrx)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if arrx[j] > arrx[j + 1]:
+                arrx[j], arrx[j + 1] = arrx[j + 1], arrx[j]
+                arry[j], arry[j + 1] = arry[j + 1], arry[j]
+
+def laGrange(xes,yes,query):
+    bubbleSortxy(xes,yes)
+    op=laGrangeHelper(xes,yes)
+    for i in range(len(query)):
+        query[i]=evaluate(op,query[i])
+    return [op,query]
+
+
+def laGrangeHelper(xes, yes):
+    op = "("
+    n=len(xes)
+    for i in range(n):
+        op=op+"(("
+        for j in range(n):
+            if j==i:
+                continue
+            op=op+"("
+            op=op+"(x-"+str(xes[j])+")/("+str(xes[i]-xes[j])+"))*"
+        op=op[:-1]
+        op=op+")*"+str(yes[i])+")+"
+    op=op[:-1]
+    op=op+")"
+    return op

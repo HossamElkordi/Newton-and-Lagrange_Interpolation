39d2ce561631bd32724454b84949d4c7603a3db1
Mina Ashraf <minachraf@outlook.com>
2020-05-02 16:54:53 +0200

    Lagrange

--- Implementaion/Interpolation.py
+++ Implementaion/Interpolation.py
@@ -22,14 +22,16 @@ class Interpolation:
         for x in X:
             for i in range(1, degree + 1):
                 y = coefficient[degree - i] + (x - xes[degree - i]) * y
-                string = '('+str(coefficient[degree - i])+'(x-'+str(xes[degree - i])+')'+string+')'
+                string = '(' + str(coefficient[degree - i]) + '(x-' + str(xes[degree - i]) + ')' + string + ')'
                 continue
             answer.append(y)
             continue
         return [answer, string]
+
+
 # https://stackoverflow.com/questions/14823891/newton-s-interpolating-polynomial-python
 # with 2 votes
-def bubbleSortxy(arrx,arry):
+def bubbleSortxy(arrx, arry):
     n = len(arrx)
     for i in range(n - 1):
         for j in range(0, n - i - 1):
@@ -37,26 +39,27 @@ def bubbleSortxy(arrx,arry):
                 arrx[j], arrx[j + 1] = arrx[j + 1], arrx[j]
                 arry[j], arry[j + 1] = arry[j + 1], arry[j]
 
-def laGrange(xes,yes,query):
-    bubbleSortxy(xes,yes)
-    op=laGrangeHelper(xes,yes)
+
+def laGrange(xes, yes, query):
+    bubbleSortxy(xes, yes)
+    op = laGrangeHelper(xes, yes)
     for i in range(len(query)):
-        query[i]=evaluate(op,query[i])
-    return [op,query]
+        query[i] = evaluate(op, query[i])
+    return [op, query]
 
 
 def laGrangeHelper(xes, yes):
     op = "("
-    n=len(xes)
+    n = len(xes)
     for i in range(n):
-        op=op+"(("
+        op = op + "(("
         for j in range(n):
-            if j==i:
+            if j == i:
                 continue
-            op=op+"("
-            op=op+"(x-"+str(xes[j])+")/("+str(xes[i]-xes[j])+"))*"
-        op=op[:-1]
-        op=op+")*"+str(yes[i])+")+"
-    op=op[:-1]
-    op=op+")"
+            op = op + "("
+            op = op + "(x-" + str(xes[j]) + ")/(" + str(xes[i] - xes[j]) + "))*"
+        op = op[:-1]
+        op = op + ")*" + str(yes[i]) + ")+"
+    op = op[:-1]
+    op = op + ")"
     return op
